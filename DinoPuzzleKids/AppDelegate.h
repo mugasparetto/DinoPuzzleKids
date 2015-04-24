1901ba7e9cf93ad9b72b468ebd0bc521a712c752
mugasparetto <mu.gasparetto@hotmail.com>
2015-04-24 14:29:15 -0300

    Initial Commit

---
+++ DinoPuzzleKids/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DinoPuzzleKids
+//
+//  Created by Murilo Gasparetto on 24/04/15.
+//  Copyright (c) 2015 Murilo Gasparetto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
